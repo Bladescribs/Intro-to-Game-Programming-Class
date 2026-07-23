9b54cec9f2bb94838d284899178657ac23b5e0d4
Bladescribs <ndajaira@gmail.com>
2026-07-23 15:48:21 -0700

    updating script

---
+++ Assets/Scripts/yuhhh.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class yuhhh : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ball(); //This is a custom function
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ball()
+    {
+        Debug.Log("Hello World"); //This is the print for the debug log of Hello World
+    }
+}
